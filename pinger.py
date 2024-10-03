1d68784ca5b3650ccd0d49d516b5b53eea43016c
EthanMorphew <ethanmorphew613@gmail.com>
2024-10-03 15:11:57 -0400

    Sliding window fixed, removing debugging print calls

--- pinger.py
+++ pinger.py
@@ -4,15 +4,18 @@
 
 import socket
 import time
+import sys
 
-#Ping Settings
+#Remote Address
 remotePort = 50555
 remoteIP = "localhost"
 remoteAddress = (remoteIP,remotePort)
 
+#Ping Settings
 pingTimout = 2
-pingWindow = pingTimout
 pingRepeats = 10
+
+pingBackoff = False
 
 #Socket Setup
 remoteSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -24,30 +27,27 @@ print("Pinging " + remoteAddress[0] + ":" + str(remoteAddress[1]) + " " + str(pi
 
 #Loops through sending udp "ping" messages 
 for i in range(pingRepeats):
-   
+   remoteSocket.settimeout(pingTimout)
+   sendTime = time.time()
    remoteSocket.sendto(("ping," + str(i)).encode(),remoteAddress)
-   pingWindow = pingTimout
-   start = time.time()
-   
+   # Loops until correct ping seq is received or timeout occurs
    while True:
       try:
-         remoteSocket.settimeout(pingWindow)
          data, recvAddress = remoteSocket.recvfrom(1024)
+         recvTime = time.time()
          pongMessage = data.decode().split(',')
          #Successful Case, print to console and send the next ping.
          if pongMessage[0] == "pong" and pongMessage[1] == str(i):
             pingSuccesses += 1
-            end = time.time()
-            rtt = int((end - start) * 1000)
+            rtt = int((recvTime - sendTime) * 1000)
             print(str(i) + ": Pong " + pongMessage[1] + " Received From " + recvAddress[0] + " RTT: " + str(rtt) + "ms")
             break
          #Received a ping with wrong seq number or garbage message, adjust timeout window and continue.
          else:
-            pingWindow = pingTimout - (time.time() - start)
-            print(pingWindow)
+            remoteSocket.settimeout(pingTimout - (recvTime - sendTime))
       #if socket timeout triggered we are over the time limit and move on to the next packet.
       except socket.timeout:
          print(str(i) + ": Request Timed Out")
          break
 
-print("Ping Success: " + str(pingSuccesses/pingRepeats*100) + "%")
+print("Ping Success: " + str(pingSuccesses/pingRepeats * 100) + "%")
--- ponger.py
+++ ponger.py
@@ -5,6 +5,7 @@
 import socket
 import random
 import time
+import sys
 
 localPort = 50555
 localIP = 'localhost'
@@ -12,14 +13,19 @@ localIP = 'localhost'
 localSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 localSocket.bind((localIP, localPort))
 
+randomDelay = False
+randomDrops = False
+
+print("Listening on " + localSocket.getsockname()[0] + ":" + str(localSocket.getsockname()[1])) 
 while True:
     try:
         data, clientAddress = localSocket.recvfrom(1024)
+
+        if randomDelay:
+            time.sleep((random.random() * 0.4) + 0.1)
+
         pingMessage = data.decode().split(',')
         if pingMessage[0] == 'ping':
-            randSleep = random.random()*3
-            print(randSleep)
-            time.sleep(randSleep)
             localSocket.sendto(('pong,'+ str(pingMessage[1])).encode(),clientAddress)
     except:
         continue
